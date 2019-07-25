574f522448e0336e108be371d4508273944163f7
dishankgoel <dishankgoel1729@gmail.com>
2019-07-25 21:04:47 +0530

    Made webpages responsive

---
+++ web1/typewrite.js
@@ -0,0 +1,15 @@
+// var i = 0;
+// var greet = 'Greetings, participant';
+// var speed = 50;
+
+// function typeWriter1() {
+//     if (i < greet.length) {
+//         document.getElementById("greet").innerHTML += greet.charAt(i);
+//         i++;
+//         setTimeout(typeWriter1, speed);
+//     }
+//     else{
+//         i = 0;
+//         speed = 30;
+//     }
+// }
